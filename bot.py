68f9780149ea376d47b4165d77dab22dfdd711a7
Kim Jin-yeong <yeong4625@gmail.com>
2021-05-20 08:26:53 +0900

    Revise timezone

--- bot.py
+++ bot.py
@@ -93,7 +93,9 @@ def alarm():
     hour = int(clock[0:2])
     minute = int(clock[2:4])
 
-    current = datetime.datetime.today()
+    KST=datetime.timezone(datetime.timedelta(hours=9))
+    current = datetime.datetime().now(tz=KST)
+
     aim = current.replace(hour=hour, minute=minute, second=00)
 
     gap = (aim - current).total_seconds()
@@ -131,7 +133,7 @@ def user_info(message):
     username_last = message.chat.last_name
     user_message = message.text
 
-    realtime=time.localtime(epochtime)
+    realtime=time.localtime(epochtime+32400)
 
     text = f"""
 USER : {username_first} {username_last}
--- notice.py
+++
@@ -1,5 +0,0 @@
-import time
-
-epoch = time.time()
-real = time.localtime(epoch)
-print(real.tm_hour)
